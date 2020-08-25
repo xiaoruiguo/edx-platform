ccca9fcd98482e498dda35a2d82e48b9019099d4
Ahtisham Shahid <ahtisham300@gmail.com>
2020-08-25 10:08:51 +0500

    Added toggle to new and old problem selector

--- lms/djangoapps/instructor/static/instructor/ProblemBrowser/components/Main/Main.jsx
+++ lms/djangoapps/instructor/static/instructor/ProblemBrowser/components/Main/Main.jsx
@@ -1,9 +1,35 @@
 /* global gettext */
-import { Button } from '@edx/paragon';
+import { Button, Icon } from '@edx/paragon';
 import BlockBrowserContainer from 'BlockBrowser/components/BlockBrowser/BlockBrowserContainer';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 import { ReportStatusContainer } from '../ReportStatus/ReportStatusContainer';
+
+// function Selector(props) {
+//   const { showBtn, handleToggleDropdown, selectedBlock } = props;
+//   if (showBtn === 'true') {
+//     return (
+//       <Button
+//         onClick={handleToggleDropdown}
+//         label={gettext('Select a section or problem')}
+//       />
+//     );
+//   }
+//   return (
+//                 // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+//     <span
+//       onClick={handleToggleDropdown}
+//       className={['problem-selector']}
+//     >
+//       <span>{selectedBlock || 'Select a section or problem'}</span>
+//       <span className={['pull-right']}>
+//         <Icon
+//           className={['fa', 'fa-sort']}
+//         />
+//       </span>
+//     </span>
+//     );
+// }
 
 export default class Main extends React.Component {
   constructor(props) {
@@ -26,37 +52,59 @@ export default class Main extends React.Component {
 
   initiateReportGeneration() {
     this.props.createProblemResponsesReportTask(
-      this.props.problemResponsesEndpoint,
-      this.props.taskStatusEndpoint,
-      this.props.selectedBlock,
-    );
+            this.props.problemResponsesEndpoint,
+            this.props.taskStatusEndpoint,
+            this.props.selectedBlock,
+        );
   }
+
 
   render() {
     const { selectedBlock, onSelectBlock } = this.props;
+    let selectorMand = <Button onClick={this.handleToggleDropdown} label={gettext('Select a section or problem')} />;
+    if (this.props.ShowBtnUi === 'false') {
+      selectorMand =
+        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+        (<span
+          onClick={this.handleToggleDropdown}
+          className={['problem-selector']}
+        >
+          <span>{selectedBlock || 'Select a section or problem'}</span>
+          <span className={['pull-right']}>
+            <Icon
+              className={['fa', 'fa-sort']}
+            />
+          </span>
+        </span>);
+    }
+
 
     return (
       <div className="problem-browser-container">
         <div className="problem-browser">
-          <Button
-            onClick={this.handleToggleDropdown}
-            label={gettext('Select a section or problem')}
+          {selectorMand}
+          <input
+            type="text"
+            name="problem-location"
+            value={selectedBlock}
+            disabled
+            hidden={this.props.ShowBtnUi === 'false'}
           />
-          <input type="text" name="problem-location" value={selectedBlock} disabled />
           {this.state.showDropdown &&
-          <BlockBrowserContainer
-            onSelectBlock={(blockId) => {
-              this.hideDropdown();
-              onSelectBlock(blockId);
-            }}
-          />}
+            <BlockBrowserContainer
+              onSelectBlock={(blockId) => {
+                this.hideDropdown();
+                onSelectBlock(blockId);
+              }}
+            />}
           <Button
             onClick={this.initiateReportGeneration}
             name="list-problem-responses-csv"
             label={gettext('Create a report of problem responses')}
           />
+          <ReportStatusContainer />
         </div>
-        <ReportStatusContainer />
+
       </div>
     );
   }
@@ -71,6 +119,7 @@ Main.propTypes = {
   onSelectBlock: PropTypes.func.isRequired,
   selectedBlock: PropTypes.string,
   taskStatusEndpoint: PropTypes.string.isRequired,
+  ShowBtnUi: PropTypes.string.isRequired,
 };
 
 Main.defaultProps = {
